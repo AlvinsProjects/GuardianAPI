e7673bbcb07df2283de076ff9c4c70bf3adf285f
Alvin Alleyne <mocojumb@gmail.com>
2025-12-08 14:50:45 -0600

    Initial Commit

---
+++ GuardianAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuardianAPI
+//
+//  Created by Alvin Alleyne on 12/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuardianAPI/GuardianAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuardianAPIApp.swift
+//  GuardianAPI
+//
+//  Created by Alvin Alleyne on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct GuardianAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
